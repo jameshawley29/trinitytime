c84ce0ced08ff00bca269b81969c2192d6896fa2
jameshawley29 <jameshawley29@gmail.com>
2022-08-26 08:43:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const userAction = async () => {
     const response = await fetch('https://us-east-1.aws.data.mongodb-api.com/app/testapp-bnqjv/endpoint/api?secret=secret');
     const myJson = await response.json(); //extract JSON from the http response
     // do something with myJson
-    document.getElementById("yourH1_element_Id").innerHTML = JSON.parse(myJson);
+    document.getElementById("demo").innerHTML = JSON.parse(myJson);
   }
