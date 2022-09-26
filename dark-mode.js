ad3fa31763a12c102557f387a4cb7117aeee0c18
jameshawley29 <jameshawley29@gmail.com>
2022-09-26 19:28:23 -0400

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -28,7 +28,6 @@ function set_dark_mode() {
     document.body.style.backgroundColor = on ? "rgb(0,0,0)" : "rgb(250,250,250)";
     document.getElementById("description").style.color = on ? "rgb(200,200,200)" : "rgb(0,0,0)";
     document.getElementById("verison-num").style.color = on ? "rgb(200,200,200)" : "rgb(0,0,0)";
-    const new_color = LightenDarkenColor(document.getElementById("clock").style.color, -20);
     //document.getElementById("clock").style.color = on ? document.getElementById("clock").style.color : new_color;
 }
 
